41de740a08615fa402e0a1ebd44980f56ca8b25a
Florian Dressler <florian.dressler@mobilemojo.de>
2018-07-30 11:11:48 +0200

    first commit

---
+++ enderecoAMS/widgets/enderecoamscssincludewidget.php
@@ -0,0 +1,46 @@
+<?php
+namespace OxidEsales\Eshop\Application\Component\Widget;
+class EnderecoAMSCSSIncludeWidget extends \OxidEsales\Eshop\Application\Component\Widget\WidgetController
+{
+
+    /**
+     * @var string Widget template
+     */
+    protected $_sThisTemplate = 'widget/enderecoamscssinputfiles.tpl';
+
+    /**
+     * Getter for protected property.
+     *
+     * @return string
+     */
+    public function getThisTemplate()
+    {
+        return $this->_sThisTemplate;
+    }
+    /**
+     *
+     * @return getTheme
+     */
+    public function getTheme()
+    {
+        return \OxidEsales\Eshop\Core\Registry::getConfig()->getConfigParam('enderecoAMSStyling');
+    }
+
+    public function insertBootstrapCSS()
+    {
+        return \OxidEsales\Eshop\Core\Registry::getConfig()->getConfigParam('enderecoAMSBootstrapModalCss');
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * Returns
+     *
+     * @return string template to render
+     */
+    public function render()
+    {
+        parent::render();
+        return $this->getThisTemplate();
+    }
+}
